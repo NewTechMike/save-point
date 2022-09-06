471499a62b9f085f1c49b5d94f38dc0389b42ea3
Michael Harris <xero5k@yahoo.com>
2022-09-06 17:53:41 -0500

    Verified Create user and session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,19 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end 
+
+  def create
+    user = User.create(user_params)
+    if user
+      render json: user, status: :created
+    else 
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end 
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end 
 end
