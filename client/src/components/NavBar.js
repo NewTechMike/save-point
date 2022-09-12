bcd234c3dbd2042c30f0875e0a49736aeb981af1
Michael Harris <xero5k@yahoo.com>
2022-09-12 15:15:20 -0500

    Cleaned up code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -20,52 +20,47 @@ function NavBar({setUser}){
   return(
     <div>
       <NavLink
-        to="/signup"
-        activeStyle={{
-          background: "darkblue",
-        }}
-        >
-          <button type="button">
-            SignUp
-          </button>
-        </NavLink>
-
-      <NavLink
         to="/welcome"
         activeStyle={{
           background: "darkblue",
-        }}
-        >
-          <button type="button">
-            Welcome
-          </button>
-        </NavLink>
+        }}>
+        <button type="button">
+          Welcome
+        </button>
+      </NavLink>
 
       <NavLink
         to="/home"
         activeStyle={{
           background: "darkblue",
-        }}
-        >
-          <button type="button">
-            Home
-          </button>
-        </NavLink>
+        }}>
+        <button type="button">
+          Home
+        </button>
+      </NavLink>
       
       <NavLink
         to="/login"
         activeStyle={{
           background: "darkblue",
-        }}
-        >
-          <button type="button">
-            Login Page
-          </button>
-        </NavLink>
+        }}>
+        <button type="button">
+          Login Page
+        </button>
+      </NavLink>
 
-        <header>
-          <button onClick={handleLogout}>Logout</button>
-        </header>
+      <NavLink
+        to="/signup"
+        activeStyle={{
+            background: "darkblue",
+        }}>
+        <button type="button">
+          SignUp
+        </button>
+      </NavLink>
+      <header>
+        <button onClick={handleLogout}>Logout</button>
+      </header>
 
     </div>
   )
