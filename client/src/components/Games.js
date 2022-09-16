6470374db17da44a9b1ea73065119c58dc13478c
Michael Harris <xero5k@yahoo.com>
2022-09-16 15:03:51 -0500

    Listing games from API

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -22,24 +22,50 @@ function Games(){
   
     const [title, setTitle] = useState()
     function handleSomething(){
+      console.log("something")
+    }
     const gameObj = {
       title: title
     }  
-    fetch('https://api.rawg.io/api/games?key=c8ab624f5d4247418c0a9614841a0791', {
 
-      method: "POST", 
-      headers: {
-        "Content-Type": "application/json"
-      }, 
-      body: JSON.stringify(gameObj)
+    const [rawgGames, setRawgGames]= useState({
+      title: "", 
+      platform: "",
+      release_date: "",
+      genre: "",
+      cover_art: ""
     })
-    .then((r)=> console.log(r))
+    useEffect(()=>{ 
+    fetch('https://api.rawg.io/api/games?key=c8ab624f5d4247418c0a9614841a0791')
+    .then((r)=> r.json())
+    .then((gameData)=>setRawgGames(
+      gameData.results
+      //platform: gameData.results[8].parent_platform[1],
+      //release_date: gameData.results[8].release_date,
+      //genre: gameData.Results[8].genres[0],
+     // cover_art: gameData.results[8].background_image
+    ))
+    },[])
 
-}
+    console.log("RG: ", rawgGames)
+
+    //<img src= {rawgData.short_screenshots[0].image} />
+      const theRawgGames = rawgGames.map((rawgData) => 
+        <li key={rawgData.id}>
+
+          <ul>{rawgData.name}</ul>   
+          <ul>{rawgData.genres[0].name}</ul>
+          <ul>{rawgData.released}</ul>
+          <ul>{rawgData.parent_platforms[0].platform.name}</ul>
+        </li> 
+      )
+
+    console.log(rawgGames)
   return (
     <div>
       <h1>Games Page</h1>
       <ul>{theGames}</ul>
+      <ul>{theRawgGames}</ul>
       <button onClick={handleSomething}>Something</button>
     </div>
   )
